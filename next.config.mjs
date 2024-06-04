8778dad91e01ee0e84f3c370253fe1152f04e796
yoosangwook <yoo32767@gmail.com>
2024-06-04 16:15:09 +0900

    cicd 긴급 테스트
    ㅅㅂ 잘 안됨!! ㅋ

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
 
-export default nextConfig;
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = { Canvas: false }
+    }
+
+    return config
+  },
+}
+
+export default nextConfig
